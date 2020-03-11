db0b66a5916b43c9ab1e6e6971ea15d0b147e235
Dave Trost <davetrost@gmail.com>
2020-03-11 02:37:05 -0700

    about me section with tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,16 +5,27 @@ import App from './App';
 describe('Site Header', () => {
   it('renders my name in the header', () => {
     const { getByText } = render(<App />);
-    const headerElement = getByText(/Dave Trost/i);
+    const headerElement = getByText('Dave Trost');
     expect(headerElement).toBeInTheDocument();
   });
   it('renders 3 categories in the navigation section', () => {
     const { getByText } = render(<App />);
-    const fullstackNav = getByText(/Full Stack/i);
+    const fullstackNav = getByText('Full Stack');
     expect(fullstackNav).toBeInTheDocument();
-    const codeChallengesNav = getByText(/Code Challenges/i);
+    const codeChallengesNav = getByText('Code Challenges');
     expect(codeChallengesNav).toBeInTheDocument();
-    const computerEngineeringNav = getByText(/Computer Engineering/i);
+    const computerEngineeringNav = getByText('Computer Engineering');
     expect(computerEngineeringNav).toBeInTheDocument();
   });
 });
+
+describe('Site Content', () => {
+  it('renders my bio in the initial state', () => {
+    const { getByText } = render(<App />);
+    const bio = getByText(/^Hi\./i);
+    const bioText = bio.textContent || '';
+    expect(bio).toBeInTheDocument();
+    expect(bioText.split(' ').length).toBeGreaterThan(50);
+  });
+});
+
--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,38 @@ function App() {
           <h2>Computer Engineering</h2>
         </nav>
       </header>
+      <section>
+        <p>
+          Hi. I've been writing code in one language or another since 2006.
+          I started a new adventure in coding in 2019 and plunged into the world of full stack software.
+          Along the way, I've learned that certain aspects of coding are universal: 
+          <span>The instant gratification when code goes from broken to fixed.</span>
+          <span>The momentum and product quality that comes from sharing ideas and learnings among developers.</span>
+          <span>Collaborative environments are fun!</span>
+        </p>
+        <p>
+          Let's get coding!
+        </p>
+      </section>
+      <section>
+        <div>
+          <p>
+            New challenges are important to me and I'm having a lot of fun practicing with the core tools of the industry. 
+            The projects below are the beginning of my explorations. 
+          </p>
+        </div>
+        <div>
+          <h3><a href='https://github.com/DaveTrost/mind-drift-ts-components'>MindDrift Breathing</a></h3>
+          <p>
+            A learning project for me to practice Typescript - converted from a prior JS project. (Credit for site architecture, graphics and animations to Abby Masters and Maeve Griffin)
+          </p>
+          <a href='https://mind-drift-breathing.netlify.com/'>Visit Site</a>
+        </div>
+        <div>
+          <h2>Skills</h2>
+          <a href='https://www.javascript.com/'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png' alt='Javascript' /></a>
+        </div>
+      </section>
     </div>
   );
 }
