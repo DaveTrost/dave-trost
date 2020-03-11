f9bfded0d6232fe10f19bc9e50d57c586c4c28f3
Dave Trost <davetrost@gmail.com>
2020-03-11 13:26:25 -0700

    component for Scrolling Bio. Tests need re-write.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollingBio from './components/ScrollingBio';
 import './App.scss';
 
 function App() {
@@ -12,52 +13,40 @@ function App() {
           <h2>Computer Engineering</h2>
         </nav>
       </header>
-      <section>
-        <h3>
-          Welcome to my coding adventures!
-        </h3>
-        <p>
-          In the last episode, I worked my way to the top of the EDA software team. I specialized in circuit design software for radio-frequency semiconductors.
-        </p>
-        <p>
-          In this episode, I plunge into the world of full stack software ...
-        </p>
-        <p>
-          The software industry offers growth and learning opportunities galore, team-mates who inspire us to bring our full selves to our work, and meaningful work-life balance.
-          And the challenges are significant: out-dated code bases, constant technology growth and intersecting domains of responsibility. 
-          It's a tall order, but I'm up to the task. I have the same work ethic that got me through the last episode, the same sharp eye for detail, and I know that no learning curve is too steep.
-        </p>
-        <p>
-          There is a theme to this series and it's that certain aspects of coding are universal: 
+      <ScrollingBio />
+      { false && 
+        <section>
+          <p>
+            If there is a theme to this series, it is that certain aspects of <span className='strikeThrough'>the force</span> coding are universal: 
+          </p>
           <ul>
             <li>We get an adrenaline boost when code goes from broken to fixed</li>
             <li>Code quality increases from sharing ideas, questions and learnings with a team</li>
             <li>Technical work is hard, collaboration is fun. We can build great things together!</li>
           </ul>
-        </p>
-        <p>
-          Let's get coding!
-        </p>
-      </section>
-      <section>
-        <div>
-          <p>
-            New challenges are important to me. I'm having fun practicing with the tools of the industry.
-            The projects below are the beginning of my explorations.
-          </p>
-        </div>
-        <div>
-          <h3><a href='https://github.com/DaveTrost/mind-drift-ts-components'>MindDrift Breathing</a></h3>
-          <p>
-            A learning project for me to practice Typescript - converted from a prior JS project. (Credit for site architecture, graphics and animations to Abby Masters and Maeve Griffin)
-          </p>
-          <a href='https://mind-drift-breathing.netlify.com/'>Visit Site</a>
-        </div>
-        <div>
-          <h2>Skills</h2>
-          <a href='https://www.javascript.com/'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png' alt='Javascript' /></a>
-        </div>
-      </section>
+        </section>
+      } 
+      { false && 
+        <section>
+          <div>
+            <p>
+              New challenges are important to me. I'm having fun practicing with the tools of the industry.
+              The projects below are the beginning of my explorations.
+            </p>
+          </div>
+          <div>
+            <h3><a href='https://github.com/DaveTrost/mind-drift-ts-components'>MindDrift Breathing</a></h3>
+            <p>
+              A learning project for me to practice Typescript - converted from a prior JS project. (Credit for site architecture, graphics and animations to Abby Masters and Maeve Griffin)
+            </p>
+            <a href='https://mind-drift-breathing.netlify.com/'>Visit Site</a>
+          </div>
+          <div>
+            <h2>Skills</h2>
+            <a href='https://www.javascript.com/'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png' alt='Javascript' /></a>
+          </div>
+        </section>
+      }
     </div>
   );
 }
---
+++ src/components/ScrollingBio.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+// import './ScrollingBio.scss';
+
+function ScrollingBio() {
+  return (
+    <section className='ScrollingBio'>
+      <p id='start'>
+        Welcome to my coding adventures!
+      </p>
+      <h1 className='episodeTitle'>
+        EPISODE 2019
+      </h1>
+      <sub>
+        Full Stack Engineering
+      </sub>
+      <div id='titles'>
+        <div id='titlecontent'>
+          <p>
+            It is a period of career transition. 
+            In the last episode, I worked my way to the top of the EDA software team. 
+            I specialized in circuit design software for radio-frequency semiconductors.
+          </p>
+          <p>
+            In this episode, I plunge into the world of full stack software. 
+            The industry is an area of great opportunity and significant challenge. 
+            I bring with me tools of past success: my work ethic, a sharp eye for detail, 
+            and a knowledge that no learning curve is too steep.
+          </p>
+          <p>
+            Success will be judged by those people I can help. 
+            Customers will use the software. 
+            I will put myself in their shoes and gain their perspective to guide my decisions.
+            I will help my teammates. Foremost, we will have fun while we work. 
+            I believe great accomplishments are a sign of even greater teamwork. 
+            I strive to bring my full self to the team 
+            and to foster an environment where this feels natural for all teammates.
+          </p>
+          <p>
+            Now let's get coding ...
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ScrollingBio;
