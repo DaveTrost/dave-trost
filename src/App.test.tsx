482760f4d26d8123090697248c221a6116069a51
Dave Trost <davetrost@gmail.com>
2020-03-11 00:42:13 -0700

    App contains my name and some categories. Passing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a header with my name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = getByText(/Dave Trost/i);
+  expect(headerElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dave Trost</h1>
+        <div>
+          <h2>Full Stack</h2>
+          <h2>Code Challenges</h2>
+          <h2>Computer Engineering</h2>
+        </div>
       </header>
     </div>
   );
