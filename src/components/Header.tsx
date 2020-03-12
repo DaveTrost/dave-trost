ff6d2b5656a6aee5a0927fc9925cee354d4663d6
Dave Trost <davetrost@gmail.com>
2020-03-11 22:49:17 -0700

    use appContainer in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,14 @@ import './Header.scss';
 function Header() {
   return (
     <header className='Header'>
-      <h1>Dave Trost</h1>
-      <nav>
-        <h2>Full Stack</h2>
-        <h2>Code Challenges</h2>
-        <h2>Computer Engineering</h2>
-      </nav>
+      <div className='appContainer'>
+        <h1>Dave Trost</h1>
+        <nav>
+          <h2>Full Stack</h2>
+          <h2>Code Challenges</h2>
+          <h2>Computer Engineering</h2>
+        </nav>
+      </div>
     </header>
   );
 }
