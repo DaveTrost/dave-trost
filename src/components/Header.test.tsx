23759d62d22cb8c5f97a21f707615988704accca
Dave Trost <davetrost@gmail.com>
2020-03-12 22:47:12 -0700

    scale back Header component until I have more time

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -9,13 +9,13 @@ describe('Site Header', () => {
     expect(headerElement).toBeInTheDocument();
   });
   
-  it('renders 3 categories in the navigation section', () => {
-    const { getByText } = render(<Header />);
-    const fullstackNav = getByText('Full Stack');
-    expect(fullstackNav).toBeInTheDocument();
-    const codeChallengesNav = getByText('Code Challenges');
-    expect(codeChallengesNav).toBeInTheDocument();
-    const computerEngineeringNav = getByText('Computer Engineering');
-    expect(computerEngineeringNav).toBeInTheDocument();
-  });
+  // it('renders 3 categories in the navigation section', () => {
+  //   const { getByText } = render(<Header />);
+  //   const fullstackNav = getByText('Full Stack');
+  //   expect(fullstackNav).toBeInTheDocument();
+  //   const codeChallengesNav = getByText('Code Challenges');
+  //   expect(codeChallengesNav).toBeInTheDocument();
+  //   const computerEngineeringNav = getByText('Computer Engineering');
+  //   expect(computerEngineeringNav).toBeInTheDocument();
+  // });
 });
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,11 @@ function Header() {
     <header className='Header'>
       <div className='appContainer'>
         <h1>Dave Trost</h1>
-        <nav>
+        {/* <nav>
           <h2>Full Stack</h2>
           <h2>Code Challenges</h2>
           <h2>Computer Engineering</h2>
-        </nav>
+        </nav> */}
       </div>
     </header>
   );
