80884583c94c0e61f5c35892553415a71aa08b4c
Dave Trost <davetrost@gmail.com>
2020-03-13 11:04:13 -0700

    fix the test

--- src/components/Bio.test.tsx
+++ src/components/Bio.test.tsx
@@ -5,7 +5,7 @@ import Bio from './Bio';
 describe('About Me section', () => {
   it('renders some information about me', () => {
     const { getByText } = render(<Bio />);
-    const headerElement = getByText(/^Hi/);
+    const headerElement = getByText(/Hi, /);
     const paragraph = getByText(/past success/);
     const paragraph2 = getByText(/shared work experience/);
     expect(headerElement).toBeInTheDocument();
