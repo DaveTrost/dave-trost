9a71191ab6b8afb9920167aef0dc6e662bccc24d
Dave Trost <davetrost@gmail.com>
2020-03-13 00:23:52 -0700

    better 'about me' story for mobile

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -6,23 +6,22 @@ function Bio() {
     <div className='appContainer bioContainer'>
       <h3>Hi, I'm Dave</h3>
       <div className='Bio'>
-        <p>
-          I am a full stack engineer and a career-changer. 
+        <p className='primary'>
+          I am a full stack engineer.
+          I gauge success by the people I help - customers and teammates alike.
+          I put myself in the customer's shoes and gain new perspectives to guide my development.
+          My teammates and I have a shared work experience. Our work should be fun! 
+          I strive to bring my full self to the team and to help others do the same. 
+          I believe that big accomplishments are the result of great teamwork.
+        </p>
+        <p className='secondary'>
+          I am a career-changer. 
           In the past, I worked my way to the top of EDA software engineering. 
           I specialized in circuit design software for radio-frequency semiconductors.
-        </p>
-        <p>
           I now call the world of full stack software my career home.
           The industry is an area of great opportunity and significant challenge. 
           I bring with me tools of past success: my work ethic, a sharp eye for detail, 
           and a knowledge that no learning curve is too steep.
-        </p>
-        <p>
-          I will judge my own success by the people I help. 
-          Customers use the software. I put myself in their shoes and gain their perspective to guide my decisions.
-          My teammates and I have a shared work experience. I want to help our work be fun! 
-          I strive to bring my full self to the team and to help others do the same. 
-          I believe that big accomplishments are the result of great teamwork.
         </p>
         <p>
           Let's work together!
