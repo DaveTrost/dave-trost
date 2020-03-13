708ba6cbd2b189662d8bfef7b043bbb5c75ccecc
Dave Trost <davetrost@gmail.com>
2020-03-13 10:24:04 -0700

    styling updates for Bio component

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -4,7 +4,7 @@ import './Bio.scss';
 function Bio() {
   return (
     <div className='appContainer bioContainer'>
-      <h3>Hi, I'm Dave</h3>
+      <h3><span>{'<'}</span>Hi, I'm Dave<span>{'>'}</span></h3>
       <div className='Bio'>
         <p className='primary'>
           I am a full stack engineer.
@@ -17,16 +17,17 @@ function Bio() {
         <p className='secondary'>
           I am a career-changer. 
           In the past, I worked my way to the top of EDA software engineering. 
-          I specialized in circuit design software for radio-frequency semiconductors.
+          I specialized in scripting of circuit design software for radio-frequency semiconductors.
           Today, I apply my coding skills in the software engineering industry.
           This industry is an area of great opportunity and significant challenge. 
-          I bring with me tools of past success: my work ethic, a sharp eye for detail, 
-          a knowledge that no learning curve is too steep (and a lot of shortcut keys!)
+          I bring with me tools of past success: a sharp eye for detail, strong problem solving & debugging, 
+          a knowledge that no learning curve is too steep, and a lot of keyboard shortcuts!
         </p>
         <p>
           Let's work together!
         </p>
       </div>
+      <h3><span>{'</'}</span>Hi<span>{'>'}</span></h3>
     </div>
   );
 }
