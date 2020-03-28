6b5d98b69cadaf1f15eda87d7bfb5e2b942f0731
Dave Trost <davetrost@gmail.com>
2020-03-28 14:08:03 -0700

    style the Episode Title: "CODE WARS"
    more work remains on the episode lead-in text "in a a galaxy far far away ..."

--- src/components/ScrollingBio.tsx
+++ src/components/ScrollingBio.tsx
@@ -2,36 +2,43 @@ import React from 'react';
 import { IBio } from './Bio';
 import './ScrollingBio.scss';
 
-const ScrollingBio = ({mission, edaBio, fullstackBio}: IBio) => (
-  <div className='ScrollingBio'>
-    <div className='appContainer relativeContainer'>
-      {/* <p id='start'>
-        Welcome to my coding adventures!
-      </p>
-      <h1 className='episodeTitle'>
-        EPISODE MMXIX
-      </h1>
-      <sub>
-        Full Stack Engineering
-      </sub> */}
-      <div id='titles'>
-        <div id='titlecontent'>
-          <p>
-            It is a period of <s>civil war</s> full stack engineering. In this space, {mission}
-          </p>
-          <p>
-            In the last episode, {edaBio}
-          </p>
-          <p>
-            In this episode, {fullstackBio} (and some Jedi mind tricks!)
-          </p>
-          <p>
-            Now let's go save the galaxy!
-          </p>
+const toRomanNumeral = require('decimal-to-roman-converter');
+
+const ScrollingBio = ({mission, edaBio, fullstackBio}: IBio) => {
+  const today = new Date();
+  const year = toRomanNumeral(today.getFullYear());
+
+  return (
+    <div className='ScrollingBio'>
+      <div className='appContainer relativeContainer'>
+        {/* <p className='episodeLeadin'>
+          In a blog post by Craig Buckler, I discovered some "force"ful CSS techniques. They are on display here ...
+        </p> */}
+        <h1 className='episodeTitle'>
+          CODE WARS
+        </h1>
+        <div id='titles'>
+          <div id='titlecontent'>
+            <p className='center'>
+              EPISODE {year}
+            </p>
+            <p>
+              It is a period of <s>civil war</s> software engineering. In this space, {mission}
+            </p>
+            <p>
+              In the last episode, {edaBio}
+            </p>
+            <p>
+              In this episode, {fullstackBio} (and some Jedi mind tricks!)
+            </p>
+            <p>
+              Now let's go save the galaxy!
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default ScrollingBio;
