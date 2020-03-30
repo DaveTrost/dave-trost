199d6f4d3b9f588e8a324c1c039bf103c5c806bb
Dave Trost <davetrost@gmail.com>
2020-03-29 21:15:55 -0700

    contact form display and hide functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
-// import Header from './components/Header';
+import React, { useState } from 'react';
+import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+ // import Header from './components/Header';
 import Bio from './components/Bio';
 import ProjectList from './containers/ProjectList';
 import SkillList from './containers/SkillList';
 import ContactSection from './components/ContactSection';
+import ContactForm from './components/ContactForm';
 import Footer from './components/FooterComp';
 import './App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Bio />
-    <ProjectList />
-    <SkillList />
-    <ContactSection />
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [showContactForm, setShowContactForm] = useState(false);
+
+  const openContactForm = () => {
+    setShowContactForm(true);
+    const modalEle = document.querySelector('.ContactForm');
+    if(modalEle) disableBodyScroll(modalEle);
+  }
+  const closeContactForm = () => {
+    setShowContactForm(false);
+    clearAllBodyScrollLocks();
+  }
+
+  return (
+    <div className='App'>
+      <ContactForm display={showContactForm} handleCancel={closeContactForm} />
+      <Bio />
+      <button onClick={openContactForm}>contact me</button>
+      <ProjectList />
+      <SkillList />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
