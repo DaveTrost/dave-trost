aaabea0dd3c0005e5c3c304b41d00238a5738298
Dave Trost <davetrost@gmail.com>
2020-04-11 14:32:15 -0700

    Animate the mission text so it comes after the typing animation

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './Mission.scss';
 import { BracketL, BracketR, Slash } from './AngleBrackets';
 
+const enterAfterTyping = 'animated fadeInUp delay-7s';
+
 const Mission = () => (
   <section className='Mission'>
     <div className='appContainer'>
@@ -13,14 +15,14 @@ const Mission = () => (
           Dave<BracketR /> <span className='cursor type-slow'> </span>
         </span>
       </h3>
-      <p>
+      <p className={`${enterAfterTyping}`}>
         I write software as an avenue to help others. 
         The software helps the customers, and that motivates my work.
         But if I'm honest with myself, software is about the connections I make while building it. 
         I love that development brings us together, and we collaborate - about the code, the product, its impact on lives ... 
         So if you code, or use software, or live life, I'm glad to help!
       </p>
-      <h3 className='monospace'>
+      <h3 className={`monospace ${enterAfterTyping}`}>
         <BracketL /><Slash />
         Hi
         <BracketR />
