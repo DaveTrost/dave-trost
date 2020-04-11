b85ab56f60235d38731695090b2f16acdcd540cf
Dave Trost <davetrost@gmail.com>
2020-04-10 23:43:41 -0700

    reusable components for '<' and '>' brackets

---
+++ src/components/AngleBrackets.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AngleBrackets.scss';
+
+export const BracketL = () => (
+  <span className='codeColor'>{'<'}</span>
+)
+export const BracketR = () => (
+  <span className='codeColor'>{'>'}</span>
+)
+export const Slash = () => (
+  <span className='codeColor'>{'/'}</span>
+)
