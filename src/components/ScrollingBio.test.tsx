240cb2d129c6b4e5b420915d83e95ad192647acd
Dave Trost <davetrost@gmail.com>
2020-04-11 02:18:41 -0700

    'Background' section - built by modifying the Bio component
    tests updated as well

--- src/components/ScrollingBio.test.tsx
+++ src/components/ScrollingBio.test.tsx
@@ -4,7 +4,6 @@ import ScrollingBio from './ScrollingBio';
 import { IBio } from './Bio';
 
 const bioProps: IBio = {
-  mission: 'help awesome people be awesome',
   edaBio: 'EDA engineering',
   fullstackBio: 'software engineering'
 }
@@ -14,10 +13,8 @@ const newRegex = (s: string) => new RegExp(s);
 describe('ScrollingBio component', () => {
   it('renders my bio in the scrolling state', () => {
     const { getByText } = render(<ScrollingBio {...bioProps} />);
-    const missionText = getByText(newRegex(bioProps.mission));
     const edaText = getByText(newRegex(bioProps.edaBio));
     const fullstackText = getByText(newRegex(bioProps.fullstackBio));
-    expect(missionText).toBeInTheDocument();
     expect(edaText).toBeInTheDocument();
     expect(fullstackText).toBeInTheDocument();
   });
