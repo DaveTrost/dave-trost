1b30d609abdd5dea0b8ded928a324cf3d5644211
Dave Trost <davetrost@gmail.com>
2020-04-13 00:10:04 -0700

    update the test for the Mission Component

--- src/components/Mission.test.tsx
+++ src/components/Mission.test.tsx
@@ -6,7 +6,10 @@ import Mission from './Mission';
 describe('Mission section', () => {
   it('renders important information', () => {
     const { getByText, getAllByText } = render(<Mission handleDoneTyping={() => 0} />);
-    mockAllIsIntersecting(true);
+    /* The Mission component goes through several setTimeout calls before completely rendering my name.
+      However, if the component leaves the screen, it is programmed to immediately render all its info.
+      Let's trick the component into rendering the title immediately for our test */
+    mockAllIsIntersecting(false);
     const titleElement = getByText(/Dave/i);
     const insideElements = getAllByText(/software/i);
     const collaborateText = getByText(/collaborate/i);
