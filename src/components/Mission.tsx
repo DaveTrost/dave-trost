f04b8d4e738f7c98443a4fd989abfbf61c32201a
Dave Trost <davetrost@gmail.com>
2020-04-12 23:58:17 -0700

    typing animation rebuilt from CSS to React in the custom hook: "useTypingAppearance"

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer'
 import { BracketL, BracketR, Slash } from './AngleBrackets';
+import { useTypingAppearance } from './useTypingAppearanceHook';
 import './Mission.scss';
 
-const typingInSeconds = 7;
-const typingTimeout = typingInSeconds*1000 + 100;
-const enterAfterTyping = `animated fadeIn fast delay-${typingInSeconds}s`;
+const message = `Hi, I'm Dave`;
+const predictiveWords = ['', '', '', '', '', '', '', '', '', 'Detailed', 'Darn Helpful', 'Dave', 'Dave'];
+const baselineTypingRate = 300;  // in milliseconds
+const predictiveTypingRate = 900;  // in milliseconds
 
 export interface IMission {
   handleDoneTyping: Function;
@@ -13,28 +15,35 @@ export interface IMission {
 
 const Mission = ({handleDoneTyping}: IMission) => {
   const [ref, inView, entry] = useInView({ threshold: .8 });
+  const [
+    isTyping, 
+    showOpeningBracket, 
+    displayText, 
+    predictiveText, 
+    showClosingBracket, 
+    endTypingAnimation
+  ] = useTypingAppearance(message, predictiveWords, baselineTypingRate, predictiveTypingRate, handleDoneTyping);
   
   /* The useEffect below is a custom bit of logic that mimics triggerOnce, but inverted.
     The useInView({triggerOnce}) modifier is sensitized to fire once on a component entering the viewport.
     However, we want to fire one time if the Mission component **exits** the viewport, so this custom thing is needed.
   */
   useEffect(() => {
-    if(entry && !inView) handleDoneTyping();
-    setTimeout(() => handleDoneTyping(), typingTimeout);
-  }, [entry, inView, handleDoneTyping]);
+    if(entry && !inView) endTypingAnimation();
+  }, 
+  [entry, inView, endTypingAnimation]);
 
   return (
     <section ref={ref} className='Mission'>
       <div className='appContainer'>
         <h3 className='monospace'>
-          <span className='typing addSpaceRight'>
-            <BracketL />Hi, I'm <span className='cursor type-fast'> </span>
-          </span>
-          <span className='typing'>
-            Dave<BracketR /> <span className='cursor type-slow'> </span>
-          </span>
+          {showOpeningBracket && <BracketL />}
+          {displayText}
+          {showClosingBracket && <BracketR />}
+          <span className={isTyping ? 'cursor' : ''}></span>
+          <span className={predictiveText ? 'predictive' : 'hiddenComponent'}>{predictiveText}</span>
         </h3>
-        <p className={`${enterAfterTyping}`}>
+        <p className={`${!isTyping ? 'animated fadeIn fast' : 'hiddenComponent'}`}>
           I write software as an avenue to help others. 
           The software helps the customers, and that motivates my work.
           And it's more than that. 
@@ -43,7 +52,7 @@ const Mission = ({handleDoneTyping}: IMission) => {
           about the code, the product, and its impact on lives. 
           So if you code, or use software, or live life, I'm glad to help! 
         </p>
-        <h3 className={`monospace ${enterAfterTyping}`}>
+        <h3 className={`monospace ${!isTyping ? 'animated fadeIn fast' : 'hiddenComponent'}`}>
           <BracketL /><Slash />
           Hi
           <BracketR />
